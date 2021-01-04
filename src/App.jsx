b456ae0f13f98d4a27f3a7cc6dc6743481683921
闫玉梅 <yanyumei@yanyumeideMacBook-Pro.local>
2021-01-04 17:34:03 +0800

    useEffect用法

--- src/App.jsx
+++ src/App.jsx
@@ -44,15 +44,43 @@ function App(props) {
   //   return props.defaultCount||0
   // });
   const [count,setCount] = useState(0);
-  //useEffect统一在渲染后调用
+  const [size,setSize] = useState({
+    width:document.documentElement.clientWidth,
+    height:document.documentElement.clientHeight,
+  })
+  const onResize = ()=>{
+    setSize({
+      width:document.documentElement.clientWidth,
+      height:document.documentElement.clientHeight,
+    })
+  }
   useEffect(()=>{
+    console.log('count',count)
+  },[count])
+  //useEffect统一在渲染后调用
+  useEffect(()=>{   //didmount，didupdate
     document.title = count;
   })
+
+  //每次渲染后都运行，加一个【】，绑定的函数只会发生在第一次渲染后，并且在组件卸载之前才会运行绑定函数
+  useEffect(()=>{    //didmount，willUnmount
+    window.addEventListener('resize',onResize);
+    return ()=>{
+      window.removeEventListener('resize',onResize);
+    }
+  },[])
   return (
     <button type="button" onClick={()=>{setCount(count+1)}}>
       click{count}
+      size:{size.width}*{size.height}
     </button>
   );
 }
 
 export default App;
+
+
+//在使用useEffext处理title和size的时候，写了两个useEffect，不同的逻辑做不同的事情，这就是关注点分离
+// 1、提高了组件复用
+// 2、优化了关注点分离
+// 两个函数的区别在于第二个参数，空数组，是精髓，只有数组的每一项都不变，所以只有第一次被渲染的时候才会被调用
