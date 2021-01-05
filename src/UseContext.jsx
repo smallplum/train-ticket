e2cb35580822e0f7de59f0b67d0f87d22c576838
闫玉梅 <yanyumei@yanyumeideMacBook-Pro.local>
2021-01-05 15:55:00 +0800

    useContext 使用

--- src/UseContext.jsx
+++ src/UseContext.jsx
@@ -1,17 +1,53 @@
-import React,{Component, useState} from 'react'
+import React,{Component, useState,createContext,useContext} from 'react'
 import './App.css';
 
+const CountContext = createContext();
+
+class Foo extends Component{
+    render(){
+        return (
+            <CountContext.Consumer>
+                {
+                    count=><h1>Foo:{count}</h1>
+                }
+            </CountContext.Consumer>
+        )
+    }
+}
+
+class Bar extends Component{
+    static contextType = CountContext;
+    render(){
+        const count = this.context;
+        return (
+            <h1>Bar:{count}</h1>
+        )
+    }
+}
+
+function Counter(){
+    const count = useContext(CountContext);
+    return (
+        <h1>Counter:{count}</h1>
+    )
+}
 
 function App(props) {
   const [count,setCount] = useState(0);
- 
   return (
-    <>
+    <div>
     <button type="button" onClick={()=>{setCount(count+1)}}>
       click{count}
     </button>
-    </>
+    <CountContext.Provider value={count}>
+        <Foo></Foo>
+        <Bar></Bar>
+        <Counter></Counter>
+    </CountContext.Provider>
+    </div>
   );
 }
 
 export default App;
+
+// ContextType作为类静态成员，不能在函数里用
