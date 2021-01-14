0ff01980deeb02b957ebb423e80a12da1dd21f07
闫玉梅 <yanyumei@yanyumeideMacBook-Pro.local>
2021-01-14 18:58:30 +0800

    异步操作的中止

--- src/component/weather/actions.js
+++ src/component/weather/actions.js
@@ -14,11 +14,42 @@ export const fetchWeatherFailure = (error)=>({
 });
 
 //关键是接下来的异步action构造函数fetchWeather
+// 在action构造函数文件中定义一个文件模块级的nextSeqId变量，
+// 这是一个递增的整数数字，给每一个访问API的请求做序列编号。
+// 在fetchWeather返回的函数中，fetch开始一个异步请求之前，
+// 先给nextSeqId自增加一，然后自增的结果赋值给一个局部变量
+// seqId，这个seqId的值就是这一次异步请求的编号，如果随后还
+// 有fetchWeather构造器被调用，那么nextSeqId也会自增，
+// 新的异步请求会分配为新的seqId。然后，action构造函数中所
+// 有的dispatch函数都被替换为一个新定义的函数dispatchIf-Valid，
+// 这个dispatchIfValid函数要检查一下当前环境的seqId是否等同于
+// 全局的nextSeqId。如果相同，说明fetchWeather没有被再次调用
+// ，就继续使用dispatch函数。如果不相同，说明这期间有新
+// 的fetchWeather被调用，也就是有新的访问服务器的请求被发出去了
+// ，这时候当前seqId代表的请求就已经过时了，直接丢弃掉，不需要
+// dispatch任何action。虽然不能真正“中止”一个API请求，但是
+// 我们可以用这种方法让一个API请求的结果被忽略，达到了中止一个
+// API请求一样的效果。在这个例子中Weather模块只有一种API请求，
+// 所以一个API调用编号序列就足够，如果需要多种API请求，则需要更
+// 多类似nextSeqId的变量来存储调用编号。
+// let a11 = 0;
+// const b11 = ++a11;
+// const c11 = ++a11;
+// console.log(a11,b11,c11)
+// 2,1,2
+
+let nextSeqId = 0;
 export const fetchWeather = (cityCode)=>{
     return (dispatch)=>{
         const apiUrl = `/data/cityinfo/${cityCode}.html`;
+        const seqId = ++nextSeqId;
+        const dispatchIfValid = (action) =>{
+            if(seqId===nextSeqId){
+                return dispatch(action)
+            }
+        }
 
-        dispatch(fetchWeatherStarted());
+        dispatchIfValid(fetchWeatherStarted());
 
         fetch(apiUrl).then((response)=>{
             if(response.status!==200){
@@ -26,12 +57,13 @@ export const fetchWeather = (cityCode)=>{
             }
 
             response.json().then((responseJson)=>{
-                dispatch(fetchWeatherSuccess(responseJson.weatherinfo));
+                dispatchIfValid(fetchWeatherSuccess(responseJson.weatherinfo));
             }).catch((error)=>{
+                dispatchIfValid(fetchWeatherFailure(error))
                 throw new Error('Invalid json response:'+error)
             });
         }).catch((error)=>{
-            dispatch(fetchWeatherFailure(error))
+            dispatchIfValid(fetchWeatherFailure(error))
         })
     }
 }
--- src/component/weather/comp/CitySelector/index.jsx
+++ src/component/weather/comp/CitySelector/index.jsx
@@ -8,6 +8,13 @@ const CITY_CODES = {
 }
 
 class index extends PureComponent {
+    // 为了防止用户选择城市请求的数据和对应借口返回的不符，最好的办法是在
+    // 发出API请求时，将之前的API请求全部中止作废，这样保证了获得有效结果是用户的最后一次选择结果。
+    // 在jquery中，可以通过abort方法取消掉一个AJAX请求
+    // const xhr = $.ajax(...);
+    // xhr.abort();
+    // fetch返回的是一个Promise对象，在ES6的标准中，Promise对象不存在中断的概念
+    // 既然fetch不能帮助我们中止api请求，就只能在应用层实现‘中断’效果，修改action构造函数
     componentDidMount(){
         const defaultCity = Object.keys(CITY_CODES)[0];
         this.props.onSelectCity(CITY_CODES[defaultCity])
